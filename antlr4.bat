4afd455be1e53f4791926ef1030d450cd763e2f1
Ruksaar Shaik <rxs1540@case.edu>
2025-11-21 17:43:40 -0500

    Stop tracking compiled files based on .gitignore

--- antlr4.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-rem Adjust JAR path if you moved the jar
-set JAR=C:\JavaLibAntlr\antlr-4.7.2-complete.jar
-
-rem Run the ANTLR Tool
-java -Xmx500M -cp "%JAR%" org.antlr.v4.Tool %*
--- grun.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-rem Adjust JAR path if you moved the jar
-set JAR=C:\JavaLibAntlr\antlr-4.7.2-complete.jar
-
-rem Run TestRig (grun) - prints tokens or runs parser
-java -cp "%JAR%;." org.antlr.v4.gui.TestRig %*
